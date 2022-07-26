0e624ee844371cb8b223cea15f6e29381359ef00
Rapheal Oke-David <raphealoke-david@Rapheals-MacBook-Air.local>
2022-07-26 12:48:59 +0200

    initial commit

---
+++ Desktop/WeatherService/weatherapi/views.py
@@ -0,0 +1,32 @@
+import urllib.request
+import json
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                        city + '&units=metric&appid=b0e9fc344b3e61957e61d44d33b13af3').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "main/index.html", data)
